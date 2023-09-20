e9338b587f5a8f14d0da3262f057f576e8c894eb
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-09-20 11:00:56 -0300

    passada a string de conexão e estabelicida ao banco Postgres

---
+++ Desenvolvimento/BackEnd/TechVagasAPI/TechVagasAPI/Controllers/CargoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechVagasAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CargoController : ControllerBase
+	{
+	}
+}
--- Desenvolvimento/BackEnd/TechVagasAPI/TechVagasAPI/Program.cs
+++ Desenvolvimento/BackEnd/TechVagasAPI/TechVagasAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TechVagasAPI.Context;
+
 namespace TechVagasAPI
 {
     public class Program
@@ -13,7 +16,12 @@ namespace TechVagasAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+			    builder.Services.AddDbContext<AppDbContext>(options =>
+							  options.UseNpgsql(connectionString));
+
+			var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
