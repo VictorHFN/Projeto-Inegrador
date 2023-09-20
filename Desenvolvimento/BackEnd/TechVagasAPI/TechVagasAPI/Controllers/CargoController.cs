8aa7ce80f7b273e033154d6c9d84a15846a2da0a
Victor Hugo Nascimento <vhfn2004@gmail.com>
2023-09-20 11:11:51 -0300

    Criado o metado GET

--- Desenvolvimento/BackEnd/TechVagasAPI/TechVagasAPI/Controllers/CargoController.cs
+++ Desenvolvimento/BackEnd/TechVagasAPI/TechVagasAPI/Controllers/CargoController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TechVagasAPI.Context;
+using TechVagasAPI.Models;
 
 namespace TechVagasAPI.Controllers
 {
@@ -7,5 +9,23 @@ namespace TechVagasAPI.Controllers
 	[ApiController]
 	public class CargoController : ControllerBase
 	{
+		private readonly AppDbContext _context;
+
+		public CargoController(AppDbContext context) 
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<CargoModel>> Get() 
+		{
+			var cargos = _context.Cargos.ToList();
+			if(cargos is null) 
+			{
+				return NotFound("Cargos não encontrados");
+			}
+			return cargos;
+		}
+
 	}
 }
